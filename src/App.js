5dd63ef74e4832661cd901febeaaa9d6ecfdbf1b
cindykhuong <cindykhuong123@hotmail.com>
2022-02-02 01:01:30 +0100

    Create a React app and deploy it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
+import { React, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import { FooterContainer } from './containers/footer';
+import { BsArrowUpLeftCircleFill } from 'react-icons/bs';
+
+/*----- Pages -----*/
 import './App.css';
+import Home from './components/pages/Home';
+import InfoSection from './components/InfoSection';
+import ImageFeed from './components/ImageFeed';
+import Kontakt from './components/Kontakt';
+import Profil from './components/Profil';
+import { CollapseContainer } from './containers/Collapse';
+import { InfoData } from './components/InfoData';
+
+/*------------ Event---------------*/
+import Tjejfika from './event/Tjejfika';
+import Ovveinvigning from './event/Ovveinvigning';
+import GaiaEvent from './event/GaiaEvent';
+import RosaVecka from './event/RosaVecka';
+import Spons from './components/Spons';
+import SökMette from './event/SökMette';
+import Damsittningen from './event/Damsittningen';
+import Märken from './event/Märken';
+
+
+
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+            {/*----- Navigation bar -----*/}
+            <Navbar/>
+
+            <ScrollToTop /> {/* Starts on top of a new page */}
+
+            {/*----- Home Page -----*/}
+            <Switch> 
+              <Route path='/' exact component={Home}/><Route/>
+            </Switch> {/*----- end Home Page -----*/}
+            
+            {/*----- "About Mette" -----*/}
+            <Switch>
+              <Route path='/' exact component= {InfoSection}>
+                <InfoSection {...InfoData}/>
+              </Route>
+            </Switch> {/*----- end "About Mette" -----*/}
+            
+            {/*----- Image feed -----*/}
+            <Switch> 
+              <Route path='/' exact component={ImageFeed}/><Route/>
+            </Switch> {/*----- end Image feed -----*/}
+    {/*  
+            ----- Sponsors -----
+            <Switch>
+              <Route path='/' exact component={Spons}/><Route/>
+            </Switch> ----- end Sponsors ----- */}
+
+            {/*----- Contact info -----*/}
+            <Switch>
+              <Route path='/kontakt' exact component={Kontakt}/><Route/>
+            </Switch> {/*----- end Contact info -----*/}
+            
+            {/*----- About current Mette -----*/}
+            <Switch>       
+              <Route path='/mette21-22' exact component={Profil}/><Route/>
+            </Switch> {/*----- end About current Mette -----*/}
+
+            {/*----- Previous Mette a.k.a Marit -----*/}
+            <Switch>
+              <Route path='/mette21-22' exact component={CollapseContainer}/><Route/>
+            </Switch> {/*----- end Previous Mette a.k.a Marit -----*/}  
+
+            {/*----- Tjejfika -----*/}
+            <Switch>
+              <Route path='/Tjejfika' exact component={Tjejfika}/><Route/>
+            </Switch> {/*----- end Tjejfika -----*/}
+
+            {/*----- Ovveinvigning -----*/}
+            <Switch>
+              <Route path='/Ovveinvigning' exact component={Ovveinvigning}/><Route/>
+            </Switch> {/*----- end Ovveinvigning -----*/}
+
+            {/*----- Gaia event -----*/}
+            <Switch>
+              <Route path='/GaiaEvent' exact component={GaiaEvent}/><Route/>
+            </Switch> {/*----- end Ovveinvigning -----*/}
+
+            {/*----- Rosa veckan event -----*/}
+            <Switch>
+              <Route path='/RosaVecka' exact component={RosaVecka}/><Route/>
+            </Switch> {/*----- end Rosa veckan -----*/}
+
+            {/*----- Sponsorer -----*/}
+            <Switch>
+              <Route path='/Spons' exact component={Spons}/><Route/>
+            </Switch> {/*----- end Spons -----*/}
+
+            {/*----- Damsittningeen -----*/}
+            <Switch>
+              <Route path='/Damsittningen' exact component={Damsittningen} /><Route />
+            </Switch> {/*----- end Damsittningen -----*/}
+
+            {/*----- Sök Mette -----*/}
+              <Switch>
+              <Route path='/SökMette' exact component={SökMette}/><Route/>
+            </Switch> {/*----- end Sök Mette -----*/}
+
+          {/*----- Märken -----*/}
+          <Switch>
+            <Route path='/Märken' exact component={Märken} /><Route />
+          </Switch> {/*----- Märken -----*/}
+
+      </Router>
+      <FooterContainer/>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/containers/footer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Footer from '../components/footer'
+import Icon from '../components/icons'
+import logga from '../images/MetteLogga.png';
+
+
+export function FooterContainer() {
+    return(
+        <Footer>
+            <Footer.Wrapper>
+                <Footer.Row>
+
+                     <Footer.Column>
+                         <img src={logga} height={160} width={160}/>
+                    </Footer.Column>
+
+                    <Footer.Column>
+                        <Footer.Title>Hitta oss</Footer.Title>
+                        <p>Linköpings Universitet på Campus Norrköping</p>
+                        <p>Bredgatan 34</p>
+                        <p>602 21 Norrköping </p>
+                    </Footer.Column>
+
+                    <Footer.Column>
+                        <Footer.Title>Om oss</Footer.Title>
+                        <Footer.Link href="/kontakt">Kontakta oss</Footer.Link>
+                        <Footer.Link href="/mette21-22">Mette</Footer.Link>
+                    </Footer.Column>
+
+                    <Footer.Column>
+                        <Footer.Title>Följ oss</Footer.Title>
+                        <Footer.Link href="https://www.facebook.com/MTMette/"><Icon className="fab fa-facebook-square"/>Facebook</Footer.Link>
+                        <Footer.Link href="https://www.instagram.com/mettemedbandana/"><Icon className="fab fa-instagram"/>Instagram</Footer.Link>
+                        {/* <Footer.Link href="#"><Icon className="fab fa-twitter"/>Twitter</Footer.Link> */}
+                    </Footer.Column>
+                </Footer.Row>
+            </Footer.Wrapper>
+        </Footer>
+    )
+}
