bcb0fb0602562559521076d6340a95e342b19d9a
emmabjelkas <93656641+emmabjelkas@users.noreply.github.com>
2022-09-17 17:31:24 +0200

    -Style.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
-import "./style.scss";
+//import "./App.js";
 
 // ReactDOM.render(
 //     <>
